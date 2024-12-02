b26f1eb4e9646f430b0248a3098b54127983c88c
Kelton <kelton.warren2@gmail.com>
2024-12-02 15:02:45 -0500

    Created code outline

---
+++ TemplateProject.cpp
@@ -0,0 +1,17 @@
+// Kelton ___
+// ________
+// December 2, 2024
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int half(int);
+template<class T> T half(T);
+
+int main() {
+
+    
+    return 0;
+}
